32e2bd9dd68c8962937ad1ec5891643873f15a07
Annie <anniebui0110@gmil.com>
2023-07-08 12:18:25 -0500

    added more stuff

---
+++ questions.js
@@ -0,0 +1,58 @@
+// Created an array passing through different value
+let questions = [
+  {
+    number: 1,
+    question: 'What is HTML stand for?',
+    answer: 'Hyper Text Markup Language',
+    options: [
+      'Hyper Text Markup Language',
+      'Hyper Text Markup Language',
+      'Hyper Text Markup Language',
+      'Hyper Text Markup Language',
+    ],
+  },
+  {
+    number: 2,
+    question: 'What does CSS stand for?',
+    answer: 'Cascading Style Sheets',
+    options: [
+      'Cascading Style Sheets',
+      'Coding Style Sheets',
+      'Cascading Stylish Sheets',
+      'Cascading Structure Sheets',
+    ],
+  },
+  {
+    number: 3,
+    question: 'What does PHP stand for?',
+    answer: 'Hypertext Preprocessor',
+    options: [
+      'Hypertext programming',
+      'Hypertext preprogramming',
+      'Hypertext Preprocessor',
+      'Hometext Preprocesssor',
+    ],
+  },
+  {
+    number: 4,
+    question: 'What does SQL stand for',
+    answer: 'Structured Query Language',
+    options: [
+      'Stylish Question Language',
+      'Stylesheet Query Language',
+      'Statement Question Language',
+      'Structured Query Language',
+    ],
+  },
+  {
+    number: 5,
+    question: 'What does XML stand for',
+    answer: 'Extensible Markup Language',
+    options: [
+      'Extensible Markup Language',
+      'Executable Multiple Language',
+      'Extra Multi-Program Language',
+      'Examine Mutliple Language',
+    ],
+  },
+];
